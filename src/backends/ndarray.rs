967cfc47ee18b548a8faf4c48635ebd8819d7966
matee8 <graves-bluff-pesky@duck.com>
2025-07-16 23:44:39 +0200

    feat(backend/ndarray): implement `NdarrayBackend`

---
+++ src/backends/ndarray.rs
@@ -0,0 +1,79 @@
+//! `ndarray` crate backend.
+
+use core::marker::PhantomData;
+
+use ndarray::{ArrayD, IxDyn};
+use num_traits::Zero;
+
+use crate::tensor::Backend;
+
+#[expect(
+    clippy::module_name_repetitions,
+    reason = "this module only defines the backend for `ndarray`."
+)]
+/// Marker type for the `ndarray` backend.
+#[derive(Debug)]
+pub struct NdarrayBackend<T>
+where
+    T: Clone,
+{
+    _marker: PhantomData<T>,
+}
+
+impl<T> Backend for NdarrayBackend<T>
+where
+    T: Clone + Zero,
+{
+    type Tensor = ArrayD<T>;
+
+    #[inline]
+    fn ndim(tensor: &Self::Tensor) -> usize {
+        tensor.ndim()
+    }
+
+    #[inline]
+    fn shape(tensor: &Self::Tensor) -> &[usize] {
+        tensor.shape()
+    }
+
+    #[inline]
+    fn zeros(shape: &[usize]) -> Self::Tensor {
+        ArrayD::zeros(IxDyn(shape))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{backends::ndarray::NdarrayBackend, tensor::Backend};
+
+    #[test]
+    fn ndarray_zeros_has_correct_shape() {
+        let shape = &[2, 3];
+        let array = NdarrayBackend::<f32>::zeros(shape);
+
+        assert_eq!(array.shape(), shape);
+    }
+
+    #[test]
+    fn ndarray_zeros_has_correct_values() {
+        let array = NdarrayBackend::<f32>::zeros(&[2, 3]);
+
+        assert!(array.iter().all(|&value| value == 0.0));
+    }
+
+    #[test]
+    fn ndarray_shape_is_correct() {
+        let shape = &[2, 3];
+        let array = NdarrayBackend::<f32>::zeros(shape);
+
+        assert_eq!(NdarrayBackend::shape(&array), shape);
+    }
+
+    #[test]
+    fn ndarray_ndim_is_correct() {
+        let shape = &[2, 3];
+        let array = NdarrayBackend::<f32>::zeros(shape);
+
+        assert_eq!(NdarrayBackend::ndim(&array), shape.len());
+    }
+}
